5a96e118cf8e71cac9454331b5eb32a1b80b5b33
shusuke-iruma-tea <shusuke.pana@gmail.com>
2023-10-12 20:50:02 +0900

    PHPのイメージサンプル

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # phpimage
+
+## イメージの作成
+- docker build . -t myphpimage
---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP IMAGE SAMPLE</title>
+    </head>
+    <body>
+        Your IP <?php echo $_SERVER['REMOTE_ADDR'] ?>.
+    </body>
+</html>
